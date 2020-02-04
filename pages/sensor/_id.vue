b5620efd6e59928320add7b2518e8f024d7e8acb
ictrobot <ictrobot@outlook.com>
2020-02-04 19:01:37 +0000

    Start bootstrap

---
+++ pages/sensor/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Sensor {{ id }} </h1>
+      </div>
+    </div>
+
+    <div class="container">
+      Hello World
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    return ["1", "2", "3", "4", "5", "6"].includes(params.id)
+  },
+  head () {
+    return {
+      title: "Sensor {{ id }}"
+    }
+  },
+  created () {
+    this.id = this.$route.params.id
+  }
+}
+</script>
+
+<style>
+
+</style>
