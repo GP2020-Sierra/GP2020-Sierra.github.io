f057ece6a663badab40024dc37fe33ad303b3e30
ictrobot <ictrobot@outlook.com>
2020-02-04 12:04:36 +0000

    Update formatting rules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,24 @@
 
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: process.env.npm_package_description || "" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
   ** Global CSS
   */
@@ -34,14 +34,14 @@ export default {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    "@nuxtjs/eslint-module"
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt'
+    "bootstrap-vue/nuxt"
   ],
   /*
   ** Build configuration
--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,17 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        sierra-web
-      </h1>
-      <h2 class="subtitle">
-        GP2020-Sierra.github.io
-      </h2>
+      <h1 class="title">sierra-web</h1>
+      <h2 class="subtitle">GP2020-Sierra.github.io</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="/flash" class="button--green">A button m9</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue"
 
 export default {
   components: {
@@ -37,7 +20,7 @@ export default {
   },
   head () {
     return {
-      title: 'Sierra'
+      title: "Sierra"
     }
   }
 }
