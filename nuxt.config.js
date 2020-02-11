73136ca73ffb1c10289bb9d9027ef026bd9059ca
Sam Kerr <sam.p.kerr@gmail.com>
2020-02-11 22:08:31 +0000

    Update bootstrap, use jquery and popper for animations

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,11 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: process.env.npm_package_description || "" }
     ],
+    script: [
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" },
+      { src: "https://code.jquery.com/jquery-3.3.1.slim.min.js" },
+      { src: "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" }
+    ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
     ]
@@ -54,7 +59,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   },
   generate: {
