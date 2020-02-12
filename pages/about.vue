045755694ef83e61ba273e33b2bbdaf0f3502ca3
Sam Kerr <sam.p.kerr@gmail.com>
2020-02-12 17:13:07 +0000

    Add an about page

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div class="p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Why it matters</h1>
+      </div>
+    </div>
+
+    <b-container>
+      <p>Hello World</p>
+    </b-container>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
