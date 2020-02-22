58014935888bd7bd659781ee1c357252b754d5da
ictrobot <ictrobot@outlook.com>
2020-02-22 14:30:53 +0000

    Cleanup and integrate real data

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,19 +58,6 @@ export default {
 
     vendor: ["vue-apexchart"],
     extend (config, ctx) {
-      const vueLoader = config.module.rules.find(
-        rule => rule.loader === "vue-loader"
-      )
-      vueLoader.options.transformToRequire = {
-        img: "src",
-        image: "xlink:href",
-        "b-img": "src",
-        "b-img-lazy": ["src", "blank-src"],
-        "b-card": "img-src",
-        "b-card-img": "img-src",
-        "b-carousel-slide": "img-src",
-        "b-embed": "src"
-      }
     }
   },
   generate: {
