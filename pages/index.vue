9bae7f06a7e179bec747f129ec9387189f761163
SamK <33693989+Haskool@users.noreply.github.com>
2020-02-24 02:24:21 +0000

    added summary based on CO2

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,11 @@
     <div class="text-center text-dark">
       <div class="p-lg-3 mx-auto my-5">
         <h2 class="display-5">Sensors overview</h2>
+        <p v-if="upToDateLocations==[]" class="display-6">We cannot recommend a place to study as our data isn't up to date :(</p>
+        <p v-else class="display-6">Based on CO2 levels, the best place to study right now is {{ recommendation.name }}</p>
+        <p v-if="recommendation.data[0].co2 < 1000" class="display-6">At just {{ recommendation.data[0].co2 }} ppm it is within the optimal CO2 range of 0 to 1000 ppm</p>
+        <p v-else class="display-6">At {{ recommendation.data[0].co2 }} ppm it is still above the optimal CO2 range of 0 to 1000 ppm</p>
+        </pclass="display-6">
       </div>
     </div>
 
@@ -23,10 +28,31 @@
 <script>
 import LocationSmall from "~/components/LocationSmall.vue"
 import Locations from "~/src/locations.js"
+import moment from "moment"
 
 export default {
   components: {
     LocationSmall
+  },
+  computed: {
+    upToDateLocations () {
+      const now = moment()
+      const locations = Object.values(this.locations)
+      return locations.filter(location =>
+        moment(location.data[0].timestamp).isAfter(now.subtract(10, "minutes")) // check if time is within 10 mins
+      )
+    },
+    recommendation () {
+      const locations = this.upToDateLocations
+      let minCo2Loc = null
+      for (let i = 0; i < locations.length; i++) {
+        const data = locations[i].data[0]
+        if (minCo2Loc == null || data.co2 < minCo2Loc.data[0].co2) {
+          minCo2Loc = locations[i]
+        }
+      }
+      return minCo2Loc
+    }
   },
   head () {
     return {
